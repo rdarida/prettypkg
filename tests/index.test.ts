a15eb5186c9c5eb0cc9d222fa93bc59ef810e455
Róbert Darida <rdarida@users.noreply.github.com>
2024-12-14 10:02:59 +0100

    test: update index.test.ts

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,4 +4,8 @@ describe('Test exports', () => {
   test('greet should be exported', () => {
     expect(greet).toBeTruthy();
   });
+
+  test('greet should return with "Hello, prettypkg!"', () => {
+    expect(greet()).toBe('Hello, prettypkg!');
+  });
 });
