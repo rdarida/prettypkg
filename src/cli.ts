2f2c7315abdd13a5002dc2363a4e142408344a61
Róbert Darida <rdarida@users.noreply.github.com>
2024-12-14 09:57:18 +0100

    chore: set up Yargs

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
+import yargs from 'yargs';
+
 import { greet } from '.';
 
-(() => {
+(argv => {
   console.log(greet());
-})();
+  console.log(argv);
+})(yargs.scriptName('prettypkg').usage('$0 [args]').help().argv);
