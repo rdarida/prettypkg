3535206d9fdf7e603c69ea83f43b89f7573e6d97
Róbert Darida <rdarida@users.noreply.github.com>
2024-12-16 19:14:00 +0100

    chore: update cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 import yargs from 'yargs';
+import { join } from 'path';
 
 import { greet } from '.';
 
 (argv => {
   console.log(greet());
   console.log(argv);
+  console.log(join(process.cwd(), 'package.json'));
 })(yargs.scriptName('prettypkg').usage('$0 [args]').help().argv);
