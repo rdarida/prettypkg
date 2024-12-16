6f3d1638d3b7e61c709673bbbaa3c65b7de7a2f8
Róbert Darida <rdarida@users.noreply.github.com>
2024-12-16 19:22:07 +0100

    feat: check package.json path

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,19 @@
 #!/usr/bin/env node
 import yargs from 'yargs';
 import { join } from 'path';
+import { existsSync } from 'fs';
 
 import { greet } from '.';
 
 (argv => {
   console.log(greet());
   console.log(argv);
+
+  const pkgPath = join(process.cwd(), 'package.json');
+
+  if (!existsSync(pkgPath)) {
+    return;
+  }
+
   console.log(join(process.cwd(), 'package.json'));
 })(yargs.scriptName('prettypkg').usage('$0 [args]').help().argv);
