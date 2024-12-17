80068324e79c614d96dbfa9fa64fa402896a0372
Róbert Darida <rdarida@users.noreply.github.com>
2024-12-17 19:10:33 +0100

    test: update format.test.ts

--- tests/format.test.ts
+++ tests/format.test.ts
@@ -4,21 +4,51 @@ describe('Test format', () => {
   test('', () => {
     const actual = JSON.stringify(
       format({
-        description: 'Hello',
+        devDependencies: {},
+        dependencies: {},
+        scripts: {},
+        files: ['dist'],
+        bin: './dist/cli.js',
+        types: './dist/index.d.ts',
+        module: './dist/index.mjs',
+        main: './dist/index.js',
+        bugs: 'www.bugs.com',
+        repository: 'www.repository.com',
+        homepage: 'www.homepage.com',
+        funding: { type: 'patreon', url: 'prettypkg' },
+        contributors: ['Jane Doe'],
+        author: 'John Doe',
+        license: 'MIT',
+        keywords: ['pretty', 'package'],
+        description: 'Description of prettypkg.',
         version: '0.0.0',
-        name: 'hello',
-        private: true,
-        author: 'John Doe'
+        name: 'prettypkg',
+        private: false
       })
     );
 
     expect(actual).toEqual(
       JSON.stringify({
-        private: true,
-        name: 'hello',
+        private: false,
+        name: 'prettypkg',
         version: '0.0.0',
-        description: 'Hello',
-        author: 'John Doe'
+        description: 'Description of prettypkg.',
+        keywords: ['pretty', 'package'],
+        license: 'MIT',
+        author: 'John Doe',
+        contributors: ['Jane Doe'],
+        funding: { type: 'patreon', url: 'prettypkg' },
+        homepage: 'www.homepage.com',
+        repository: 'www.repository.com',
+        bugs: 'www.bugs.com',
+        main: './dist/index.js',
+        module: './dist/index.mjs',
+        types: './dist/index.d.ts',
+        bin: './dist/cli.js',
+        files: ['dist'],
+        scripts: {},
+        dependencies: {},
+        devDependencies: {}
       })
     );
   });
