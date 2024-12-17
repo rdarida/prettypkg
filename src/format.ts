f6dd22806ee6bf7e32d6579bfb1f336aaef1872c
Róbert Darida <rdarida@users.noreply.github.com>
2024-12-17 16:50:17 +0100

    feat: add default formatter function

--- src/format.ts
+++ src/format.ts
@@ -5,14 +5,28 @@ type PartialPackageJSON = Partial<PackageJSON>;
 const PACKAGE_JSON_KEYS: (keyof PackageJSON)[] = [
   'private',
   'name',
-  'version'
+  'version',
+  'description',
+  'author'
 ];
+
+type Formatter = (value: any) => any;
+
+function getFormatter(key: keyof PackageJSON): Formatter {
+  switch (key) {
+    default:
+      return (value: any) => value;
+  }
+}
 
 export function format(pkgObj: PartialPackageJSON): PartialPackageJSON {
   let prettyPkg: PartialPackageJSON = {};
 
   PACKAGE_JSON_KEYS.forEach(key => {
-    const value = pkgObj[key];
+    if (!(key in pkgObj)) return;
+
+    const formatter = getFormatter(key);
+    const value = formatter(pkgObj[key]);
     delete pkgObj[key];
 
     if (value !== undefined) {
