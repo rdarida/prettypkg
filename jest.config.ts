14c1e9b403c19d6f6b358d85b93c22e562eaac7e
Róbert Darida <rdarida@users.noreply.github.com>
2026-06-11 21:43:45 +0200

    chore: upgrade Jest to v30

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+/** @jest-config-loader ts-node */
+import { defaults, defineConfig } from 'jest-config';
+
+export default defineConfig({
+  collectCoverage: false, // npm test -- --collectCoverage
+  collectCoverageFrom: ['./src/**/*.{ts,tsx}', '!./src/**/*.d.ts'],
+  coveragePathIgnorePatterns: [
+    ...defaults.coveragePathIgnorePatterns,
+    './dist/',
+    '/src/cli.ts',
+    './tests/'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  }
+});
